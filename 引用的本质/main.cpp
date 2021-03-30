b0a281a4d6caf8c2ce9d7174f67fefb44073a39f
dongshilin <dongshilin@bestitu.com>
2021-03-30 23:06:35 +0800

    学习引用 a b地址相同

--- 引用的本质/main.cpp
+++ 引用的本质/main.cpp
@@ -6,6 +6,19 @@ int main(int argc, char* argv[])
 {
 	cout <<"hello"<<endl;
 
+
+	int a = 10;
+	int &b = a;
+
+	cout <<"a = "<<a<<"; b = "<<b<<endl;
+
+	cout <<"input b"<<endl;
+
+	cin >>b;
+
+	cout <<"a = "<<a<<"; b = "<<b<<endl;
+	cout <<&b<<endl<<&a<<endl;
+
 	system("pause");
 	return 0;
 }
